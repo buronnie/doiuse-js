fb8fe28c1f410fde7d83cf4440b81a075708bc58
kai.guo <kai.guo@servicenow.com>
2018-07-30 22:41:02 -0700

    add more es6 syntax checks

--- data/features.js
+++ data/features.js
@@ -1,3 +1,9 @@
 module.exports = [
 	'arrow-functions',
+	'template-literals',
+	'es6-class',
+	'es6-generators',
+	'es6-module',
+	'es6-module-dynamic-import',
+	'const',
 ];
--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 var foo = () => {};
+var a = `123`;
+class Foo {
+	constructor() {
+      super(); 
+    }
+}
+
+import { a } from 'foo.js';
+import b from 'foo.js';
+export default Foo;
+export function bar() {};
+import('a.js').then(b=>{});
+
+const m = 1;
